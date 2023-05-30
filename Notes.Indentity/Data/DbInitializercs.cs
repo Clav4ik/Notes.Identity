564ca1e4799c00a7a480c7bae6f1a7405f198ca3
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2023-05-30 14:03:26 +0300

    Добавьте файлы проекта.

---
+++ Notes.Indentity/Data/DbInitializercs.cs
@@ -0,0 +1,13 @@
+﻿using Notes.Identity.Data;
+
+namespace Notes.Identity.Data
+{
+    public static class DbInitializercs
+    {
+        public static void Initialize(AuthDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+        
+    }
+}
